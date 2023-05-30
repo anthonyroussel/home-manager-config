7d0716970ac04eef85cf144dc934dd22ea5d829e
Anthony Roussel <anthony@roussel.dev>
2023-05-30 22:44:36 +0200

    chore: bump stateVersion to 23.05

--- home.nix
+++ home.nix
@@ -93,7 +93,7 @@ in
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.11";
+  home.stateVersion = "23.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
