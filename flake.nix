058a9f04b64f4618d113d5fe160a470a6633a125
Anthony Roussel <anthony@roussel.dev>
2023-05-30 22:06:21 +0200

    Add devenv, nur & switch to nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,22 +3,32 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    devenv = {
+      url = "github:cachix/devenv/latest";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, nur, devenv, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations.aroussel = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home.nix ];
+        modules = [
+          nur.hmModules.nur
+          ./home.nix
+        ];
+        extraSpecialArgs = { inherit nur devenv; };
       };
     };
 }
