853cd20f1c2f31f607db0b012965a4969dce52e8
Anthony Roussel <anthony@roussel.dev>
2023-06-29 12:10:30 +0200

    feat(brave): add Proton Pass extension to Brave

--- packages/brave/default.nix
+++ packages/brave/default.nix
@@ -5,6 +5,7 @@
       { id = "gighmmpiobklfepjocnamgkkbiglidom"; } # AdBlock — best ad blocker
       { id = "jpmkfafbacpgapdghgdpembnojdlgkdl"; } # AWS Extend Switch Roles
       { id = "fdjamakpfbbddfjaooikfcpapjohcfmg"; } # Dashlane
+      { id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # Proton Pass
     ];
   };
 }
