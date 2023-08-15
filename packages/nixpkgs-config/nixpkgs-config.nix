7f88ca912ba0ce22aa618f5ec4aa4e210cad08c0
Anthony Roussel <anthony@roussel.dev>
2023-08-15 07:58:54 +0200

    chore: update permittedInsecurePackages package list

--- packages/nixpkgs-config/nixpkgs-config.nix
+++ packages/nixpkgs-config/nixpkgs-config.nix
@@ -1,7 +1,10 @@
 {
   permittedInsecurePackages = [
     "openssl-1.1.1t"
-    "nodejs-16.20.0"
+    "nodejs-16.20.1"
     "ruby-2.7.8"
+    "ruby-2.7.3"
+    "ngrok-3.3.0"
+    "openssl-1.1.1u"
   ];
 }
