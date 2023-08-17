636b6fbb9a73bbba6c04eec0e803f9cf5e72904e
Anthony Roussel <anthony@roussel.dev>
2023-08-17 13:15:34 +0200

    feat(gpg): import my PGP public keys

--- packages/gpg/default.nix
+++ packages/gpg/default.nix
@@ -1,6 +1,16 @@
 {
   programs.gpg = {
     enable = true;
+    publicKeys = [
+      {
+        source = ./pubkeys/9DC4987B1A55E75E.asc;
+        trust = "ultimate";
+      }
+      {
+        source = ./pubkeys/379FF498F4BDCCD9.asc;
+        trust = "ultimate";
+      }
+    ];
   };
 
   services.gpg-agent = {
