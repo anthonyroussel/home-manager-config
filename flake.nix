c1c92fbd12b5397c6ad7d85082d2e9131f1e3d72
Anthony Roussel <anthony@roussel.dev>
2023-08-17 11:35:18 +0200

    feat(packages): add nixpkgs-review-checks package

--- flake.nix
+++ flake.nix
@@ -9,12 +9,16 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs-review-checks = {
+      url = "github:SuperSandro2000/nixpkgs-review-checks";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nur = {
       url = "github:nix-community/NUR";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nur, ... }:
+  outputs = inputs @ { self, nixpkgs, home-manager, nur, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -23,15 +27,24 @@
         inherit (pkgs);
         imports = [
           nur.hmModules.nur
+          ./modules/awscli.nix
           ./home.nix
         ];
       };
 
       homeConfigurations.aroussel = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+
         modules = [
           self.homeManagerModules.aroussel
           {
+            nixpkgs.overlays = [
+              (
+                final: prev: {
+                  nixpkgs-review-checks = inputs.nixpkgs-review-checks.packages."${system}".nixpkgs-review-checks;
+                }
+              )
+            ];
             home.username = "aroussel";
             home.homeDirectory = "/home/aroussel";
           }
--- home.nix
+++ home.nix
@@ -81,6 +81,7 @@ in
 
     config.nur.repos.anthonyroussel.shadow-prod
     nixpkgs-review
+    nixpkgs-review-checks
 
     calibre
     starship
