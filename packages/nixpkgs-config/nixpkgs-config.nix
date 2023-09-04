5f0177def754e435f214499abbdc1d06dcd040c9
Anthony Roussel <anthony@roussel.dev>
2023-09-04 11:49:09 +0200

    fix(nixpkgs-config): remove terrible permitted insecure packages

--- packages/nixpkgs-config/nixpkgs-config.nix
+++ packages/nixpkgs-config/nixpkgs-config.nix
@@ -1,10 +1,3 @@
 {
-  permittedInsecurePackages = [
-    "openssl-1.1.1t"
-    "nodejs-16.20.1"
-    "ruby-2.7.8"
-    "ruby-2.7.3"
-    "ngrok-3.3.0"
-    "openssl-1.1.1u"
-  ];
+  permittedInsecurePackages = [];
 }
