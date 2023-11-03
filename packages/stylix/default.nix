159b3bc7222f60e3e6bb9d03c44296552f71ace5
Anthony Roussel <anthony@roussel.dev>
2023-11-03 16:00:45 +0100

    feat(stylix): add cursor, fonts, opacity, image

--- packages/stylix/default.nix
+++ packages/stylix/default.nix
@@ -6,8 +6,21 @@
   };
 
   stylix = {
-    image = ./NixOS-Gradient-grey.png;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/solarflare.yaml";
+
+    image = pkgs.fetchurl {
+      url = "https://cdnb.artstation.com/p/assets/images/images/016/252/301/4k/grady-frederick-atlantis-garbageman-v2.jpg";
+      hash = "sha256-J4CCXMV698POytE0HAxjOlBW7mO+uSr+Fqy585G/LF8=";
+    };
+
     polarity = "dark";
+
+    opacity = {
+      terminal = 0.90;
+      applications = 0.90;
+      popups = 0.50;
+      desktop = 0.90;
+    };
 
     fonts = {
       serif = {
@@ -28,7 +41,18 @@
       sizes = {
         applications = 11;
         desktop = 11;
+        terminal = 14;
       };
+    };
+
+    targets = {
+      vscode.enable = false;
+    };
+
+    cursor = {
+      name = "phinger-cursors-light";
+      package = pkgs.phinger-cursors;
+      size = 32;
     };
   };
 }
