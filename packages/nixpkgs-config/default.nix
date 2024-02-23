b4c43a906ff51a7e7acb3404e9615b666e0e4a45
Anthony Roussel <anthony@roussel.dev>
2024-02-23 13:47:13 +0100

    feat(nixpkgs-config): update allowUnfreePredicate

--- packages/nixpkgs-config/default.nix
+++ packages/nixpkgs-config/default.nix
@@ -3,16 +3,23 @@
 {
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "android-studio-stable"
+    "cudatoolkit"
     "discord"
     "google-chrome"
     "microsoft-edge-stable"
+    "ruby-mine"
     "shadow-prod"
     "spotify-unwrapped"
     "spotify"
+    "steam"
+    "steamcmd"
+    "steam-original"
+    "steam-run"
     "vscode-extension-github-codespaces"
     "vscode-extension-ms-vscode-remote-remote-containers"
     "vscode-extension-ms-vsliveshare-vsliveshare"
     "vscode"
+    "webstorm"
   ];
   xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
 }
