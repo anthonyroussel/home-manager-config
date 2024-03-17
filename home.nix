2404d236b41337e0294a0f79f5869bcaa6aa44f3
Anthony Roussel <anthony@roussel.dev>
2024-03-17 21:20:08 +0100

    feat(packages): move from home.nix

--- home.nix
+++ home.nix
@@ -1,28 +1,8 @@
 { pkgs, config, ... }:
 
-let
-  aws = with pkgs; [
-    awsebcli
-    aws-sso-cli
-    eksctl
-    s3cmd
-    awslogs
-  ];
-
-  databases = with pkgs; [
-    postgresql_16
-    redis
-    usql
-  ];
-
-  networking = with pkgs; [
-    dig
-    mtr
-    nload
-  ];
-in
 {
   imports = [
+    ./packages.nix
     ./packages/awscli
     ./packages/brave
     ./packages/desktop
@@ -48,81 +28,6 @@ in
     ./packages/xsession
     ./packages/zsh
   ];
-
-  # Packages that should be installed to the user profile.
-  home.packages = (with pkgs; [
-    acpi
-    anki
-    bintools
-    borgbackup
-    bruno
-    calibre
-    chezmoi
-    discord
-    dislocker
-    espeak
-    file
-    filezilla
-    ffmpeg-full
-    gimp
-    gopass
-    goss
-    homebank
-    httpie
-    hydra-check
-    imagemagick
-    inkscape
-    jetbrains.ruby-mine
-    jetbrains.webstorm
-    killall
-    libsForQt5.gwenview
-    libwebp
-    mdcat
-    mplayer
-    mpv
-    mupdf
-    nix
-    nix-health
-    nixfmt
-    nixpkgs-fmt
-    nixpkgs-hammering
-    (nixpkgs-review.override {
-      withSandboxSupport = true;
-      withNom = true;
-    })
-    nixpkgs-review-checks
-    nvtop
-    oath-toolkit
-    openssh
-    p7zip
-    pcsx2
-    podman
-    powerline-fonts
-    protonvpn-cli
-    protonvpn-gui
-    pup
-    rclone
-    shadow-prod
-    simplescreenrecorder
-    sops
-    sox
-    starship
-    steam-tui
-    steamguard-cli
-    steampipe
-    tig
-    tomb
-    topgrade
-    transmission-qt
-    tree
-    unzip
-    upscayl
-    whatsapp-for-linux
-    xclip
-    yq-go
-    zbar
-    zellij
-  ]) ++ aws ++ databases ++ networking;
 
 
   # This value determines the Home Manager release that your
---
+++ packages.nix
@@ -0,0 +1,104 @@
+{ pkgs, ... }:
+
+let
+  aws = with pkgs; [
+    awsebcli
+    aws-sso-cli
+    eksctl
+    s3cmd
+    awslogs
+  ];
+
+  databases = with pkgs; [
+    postgresql_16
+    redis
+    usql
+  ];
+
+  networking = with pkgs; [
+    dig
+    mtr
+    nload
+  ];
+in
+{
+  # Packages that should be installed to the user profile.
+  home.packages =
+    (with pkgs; [
+      acpi
+      anki
+      bintools
+      borgbackup
+      bruno
+      calibre
+      chezmoi
+      discord
+      dislocker
+      espeak
+      file
+      filezilla
+      ffmpeg-full
+      gimp
+      gopass
+      goss
+      homebank
+      httpie
+      hydra-check
+      imagemagick
+      inkscape
+      jetbrains.ruby-mine
+      jetbrains.webstorm
+      killall
+      libsForQt5.gwenview
+      libwebp
+      lm_sensors
+      mdcat
+      mplayer
+      mpv
+      mupdf
+      nix
+      nix-health
+      nixfmt
+      nixpkgs-fmt
+      nixpkgs-hammering
+      (nixpkgs-review.override {
+        withSandboxSupport = true;
+        withNom = true;
+      })
+      nixpkgs-review-checks
+      nvtop
+      oath-toolkit
+      p7zip
+      pcsx2
+      podman
+      powerline-fonts
+      protonvpn-cli
+      protonvpn-gui
+      pup
+      rclone
+      shadow-prod
+      simplescreenrecorder
+      sops
+      sox
+      starship
+      steam-tui
+      steamguard-cli
+      steampipe
+      tig
+      tomb
+      topgrade
+      transmission-qt
+      tree
+      unzip
+      upscayl
+      vlc
+      whatsapp-for-linux
+      xclip
+      yq-go
+      zbar
+      zellij
+    ])
+    ++ aws
+    ++ databases
+    ++ networking;
+}
