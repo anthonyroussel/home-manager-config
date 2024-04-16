a52015980138b61267385ddcb35df9c6b3a9a134
Anthony Roussel <anthony@roussel.dev>
2024-04-16 19:24:37 +0200

    feat(gpg): rm gpg-agent

--- packages/gpg/default.nix
+++ packages/gpg/default.nix
@@ -12,19 +12,4 @@
       }
     ];
   };
-
-  services.gpg-agent = {
-    enable = true;
-
-    # Set the maximum time a cache entry is valid.
-    maxCacheTtl = 3600;
-
-    # Set the maximum time a cache entry used for SSH keys is valid.
-    maxCacheTtlSsh = 3600;
-
-    # Use pinentry-gnome3 as the PIN entry.
-    pinentryFlavor = "gnome3";
-
-    enableSshSupport = true;
-  };
 }
