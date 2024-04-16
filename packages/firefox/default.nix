89e4080dd1b4433dc07cbcdeaef3e76fb36a5a25
Anthony Roussel <anthony@roussel.dev>
2024-04-16 19:26:43 +0200

    feat(firefox): disable some trackers

--- packages/firefox/default.nix
+++ packages/firefox/default.nix
@@ -1,15 +1,36 @@
+{ pkgs, ... }:
+
 {
   programs.firefox = {
     enable = true;
-    nativeMessagingHosts = [
-      pkgs.fx-cast-bridge
-    ];
+    nativeMessagingHosts = [ pkgs.fx-cast-bridge ];
     profiles = {
       default = {
         name = "default";
-        settings = [
+        settings = {
+          # See https://github.com/nixypanda/dotfiles/blob/master/modules/firefox/config/annoyances.nix
+          # Disable the first run tabs with advertisements for the latest firefox features.
+          "browser.startup.homepage_override.mstone" = "ignore";
 
-        ];
+          # Disable new tab page intro
+          "browser.newtabpage.introShown" = false;
+
+          # Disable Sponsored Top Sites
+          "services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSite" = false;
+
+          # Do not trim URLs in navigation bar
+          "browser.urlbar.trimURLs" = false;
+
+          # Disable checking if Firefox is the default browser
+          "browser.shell.checkDefaultBrowser" = false;
+
+          # Enable Do-not-Track
+          "privacy.donottrackheader.enabled" = true;
+          "privacy.donottrackheader.value" = 1;
+
+          # Use KDE File picker using XDG Desktop portal
+          "widget.use-xdg-desktop-portal.file-picker" = 1;
+        };
       };
     };
   };
