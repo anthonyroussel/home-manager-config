9996311fd1872738cd159b2670581e01ee60761d
Anthony Roussel <anthony@roussel.dev>
2024-05-03 15:38:49 +0200

    fix(shells): add terraform to allowUnfreePredicate

--- shells/flake-module.nix
+++ shells/flake-module.nix
@@ -2,8 +2,18 @@
 
 {
   perSystem =
-    { inputs', pkgs, ... }:
     {
+      inputs',
+      pkgs,
+      system,
+      ...
+    }:
+    {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "terraform" ];
+      };
+
       devShells = pkgs.callPackage ./default.nix { };
     };
 }
