c9b25c38337890e4a7be015f5ec70bb0b049dbe2
Anthony Roussel <anthony@roussel.dev>
2025-12-11 21:25:36 +0100

    gpg: update public keys

--- home-manager/home/modules/gpg/default.nix
+++ home-manager/home/modules/gpg/default.nix
@@ -3,12 +3,29 @@
     enable = true;
     publicKeys = [
       {
-        source = ./pubkeys/9DC4987B1A55E75E.asc;
+        # anthony@roussel.dev (created: 2025-12-11)
+        source = ./pubkeys/AEB91B2BBEBC627E.asc;
         trust = "ultimate";
       }
       {
+        # anthony@roussel.dev (created: 2022-08-05, expired: 2024-08-04)
+        source = ./pubkeys/9DC4987B1A55E75E.asc;
+        trust = "never";
+      }
+      {
+        # anthony.roussel17@gmail.com (created: 2021-10-16, expired: 2026-10-15)
         source = ./pubkeys/379FF498F4BDCCD9.asc;
-        trust = "ultimate";
+        trust = "never";
+      }
+      {
+        # anthony.roussel17@gmail.com (created: 2017-09-26, revoked: 2021-10-16)
+        source = ./pubkeys/69661D2F99BE26B6.asc;
+        trust = "never";
+      }
+      {
+        # a.roussel53@outlook.com (created: 2014-12-11, revoked: 2021-10-16)
+        source = ./pubkeys/8C59046557CBC26E.asc;
+        trust = "never";
       }
     ];
   };
